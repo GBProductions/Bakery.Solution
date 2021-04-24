cd5273ac575429b84355dab9b4ce667cd583a908
Garrett Brown <garrettpaulbrown@gmail.com>
2021-04-24 16:44:15 -0700

    Create console lines.

---
+++ Bakery/Bakery.cs
@@ -0,0 +1,16 @@
+using System;
+// using Bakery.models;
+using System.Collections.Generic;
+
+namespace Bakery
+{
+    public class BakeryProgram
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Pierre's Bakery!");
+            Console.WriteLine("Bread is $5. OR buy 2, get 1 free.");
+            Console.WriteLine("Pastry is $2. OR buy 3 for $5.");
+        }
+    }
+}
