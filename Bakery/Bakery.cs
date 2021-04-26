64473460ef9bccfd803e5b98627abe474e68b3f8
Garrett Brown <garrettpaulbrown@gmail.com>
2021-04-26 11:15:14 -0700

    Build out console for UI.

--- Bakery/Bakery.cs
+++ Bakery/Bakery.cs
@@ -12,6 +12,16 @@ namespace Bakery
             Console.WriteLine("Welcome to Pierre's Bakery!");
             Console.WriteLine("Bread is $5. OR buy 2, get 1 free.");
             Console.WriteLine("Pastry is $2. OR buy 3 for $5.");
+
+            Console.WriteLine("How many loaves of Bread do you want? Enter a number please.");
+            string stringBread = Console.ReadLine();
+            int bread = int.Parse(stringBread);
+
+            Console.WriteLine("How many pastries do you want? Enter a number please.");
+            string stringPastry = Console.ReadLine();
+            int pastry = int.Parse(stringPastry);
+
+            Console.WriteLine("You want " + bread + " loaves and " + pastry + " individual pastries.");
         }
     }
 }
