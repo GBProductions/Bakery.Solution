04a578599a796d45867b9dfd60f841e5e41c9b10
Garrett Brown <garrettpaulbrown@gmail.com>
2021-04-26 17:11:06 -0700

    Bread is 3 for 5 pass.

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -4,7 +4,19 @@ namespace Bread.Models
     {
         public static int BreadIs(int bread)
         {
-            return (bread * 5);
+            if (bread <= 2) {
+                return (bread * 5);
+            } else {
+                var breadRemainders = (bread % 3);
+                var noDeal = (breadRemainders * 5);
+
+                var breadOrder = (bread - breadRemainders);
+                var preDiscountOrder = (bread * 5);
+                var finalOrder = (preDiscountOrder - ((breadOrder / 3) * 5));
+                
+
+                return (noDeal + finalOrder);
+            }
         }
     }
 }
